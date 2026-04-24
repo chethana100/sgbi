48cfd883273e71c4d57d4402407ffe85b8cd07fc
chethana100 <chethananair100@gmail.com>
2026-04-24 11:05:24 +0530

    fix: remove next-pwa webpack config, use turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  turbopack: {},
+};
 
-module.exports = withPWA(nextConfig);
+export default nextConfig;
